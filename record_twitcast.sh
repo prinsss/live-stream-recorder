e3703e4de74eb66cf702c29ddd360e7f6becb8dc
printempw <h@prinzeugen.net>
2018-12-22 15:11:55 +0800

    Use livedl as default recorder for TwitCasting

--- record_twitcast.sh
+++ record_twitcast.sh
@@ -6,15 +6,19 @@ if [[ ! -n "$1" ]]; then
   exit 1
 fi
 
-# Specify "video=1" to get video & audio stream
-M3U8_URL="http://twitcasting.tv/$1/metastream.m3u8?video=1"
+if [[ ! -f "./livedl" ]]; then
+  echo "This script depends on livedl (https://github.com/himananiito/livedl)."
+  echo "Please put the binary file of livedl in the same directory first."
+  exit 1
+fi
 
 while true; do
   # Monitor live streams of specific user
   while true; do
     LOG_PREFIX=$(date +"[%Y-%m-%d %H:%M:%S]")
     echo "$LOG_PREFIX Try to get current live stream of twitcasting.tv/$1"
-    (curl -s "$M3U8_URL" | grep -q "#EXTM3U") && break
+    STREAM_API="https://twitcasting.tv/streamserver.php?target=$1&mode=client"
+    (curl -s "$STREAM_API" | grep -q '"live":true') && break
 
     echo "$LOG_PREFIX The stream is not available now."
     echo "$LOG_PREFIX Retry after 30 seconds..."
@@ -26,16 +30,12 @@ while true; do
   echo "$LOG_PREFIX Start recording, stream saved to \"$FNAME\"."
   echo "$LOG_PREFIX Use command \"tail -f $FNAME.log\" to track recording progress."
 
-  # Record high resolution stream using livedl
-  # Build livedl from https://github.com/himananiito/livedl and
-  # put the binary file to the same directory of this script to enable.
-  if [[ -f "./livedl" ]]; then
-    ./livedl -tcas "$1" > "$FNAME.livedl.log" 2>&1 &
-    echo "$LOG_PREFIX Start recording high resolution stream simultaneously with livedl."
-  fi
+  # Also record low resolution stream simultaneously as backup
+  M3U8_URL="http://twitcasting.tv/$1/metastream.m3u8?video=1"
+  ffmpeg -i "$M3U8_URL" -codec copy -f mpegts "$FNAME" > "$FNAME.log" 2>&1 &
 
   # Start recording
-  ffmpeg -i "$M3U8_URL" -codec copy -f mpegts "$FNAME" > "$FNAME.log" 2>&1
+  ./livedl -tcas "$1" > "$FNAME.livedl.log" 2>&1
 
   # Exit if we just need to record current stream
   LOG_PREFIX=$(date +"[%Y-%m-%d %H:%M:%S]")
